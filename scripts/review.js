bfc46556013b851b80f16fc0468b2ddf3e5239c1
enriqueic <iba24001@byui.edu>
2024-07-24 21:36:00 -0700

    finished review

--- scripts/review.js
+++ scripts/review.js
@@ -1,12 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
     let reviewCount = localStorage.getItem("review-count");
     if (!reviewCount) {
         reviewCount = 0;
     }
     reviewCount++;
-    localStorage.setItem("reviewCount", reviewCount);
-    document.getElementById("reviewCount").textContent = reviewCount;
-
-    document.getElementById('currentyear').textContent = new Date().getFullYear();
-    document.getElementById('lastModified').textContent = 'Last modified: ' + document.lastModified;
+    localStorage.setItem("review-count", reviewCount);
+    document.getElementById("review-count").textContent = reviewCount;
 });
